720d4d0b4e9b1616105a7cf61d20879884b307a0
Admin <jareddowning@d-172-25-42-52.dhcp.virginia.edu>
2017-03-22 16:10:31 -0400

    init

---
+++ YakYik/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  YakYik
+//
+//  Created by Admin on 3/22/17.
+//  Copyright © 2017 Downing Development. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YakYik/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  YakYik
+//
+//  Created by Admin on 3/22/17.
+//  Copyright © 2017 Downing Development. All rights reserved.
+//
+
+import Foundation
